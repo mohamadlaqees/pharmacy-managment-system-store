baf143591724ab8a394c7bf0b3ab91704a5a6f08
Elyas <hamedahty2000@gmail.com>
2023-05-28 19:41:41 +0200

    Add product tile to cart page

--- src/pages/Purchases.js
+++ src/pages/Purchases.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import ProductTile from "../Components/ProductTile";
+import { Col, Container, Row } from "react-bootstrap";
 
 function Purchases() {
   return (
-    <div className='page'>Purchases</div>
-  )
+    <div className="page">
+      <Container class="mt-10">
+        <Row>
+          <Col xs={12} md={3}>address data</Col>
+          <Col md={9} xs={12}>
+            <ProductTile />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Purchases
+export default Purchases;
