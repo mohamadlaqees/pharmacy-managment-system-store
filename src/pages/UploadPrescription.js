af1843e92c4a39b753371bdee9e55882bd8eb1a5
Elyas <hamedahty2000@gmail.com>
2023-05-29 20:10:37 +0200

    ImageUpload component

--- src/pages/UploadPrescription.js
+++ src/pages/UploadPrescription.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import AddressData from "../Components/AddressData";
+import ImageUploader from "../Components/ImageUploader";
+import { Fragment } from "react";
 
 function UploadPrescription() {
-  return <div className="page">UploadPrescription</div>;
+  return (
+    <div className="page">
+    <ImageUploader />
+    </div>
+  );
 }
-
 export default UploadPrescription;
