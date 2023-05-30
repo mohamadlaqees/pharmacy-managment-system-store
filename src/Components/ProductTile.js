913d6019e2f90e83969e5ed60ef4eaa3f69562e7
mohamadlaqees <mohamadlk24@gmail.com>
2023-05-30 16:33:39 +0300

    .

--- src/Components/ProductTile.js
+++ src/Components/ProductTile.js
@@ -15,7 +15,11 @@ function ProductTile() {
                 Dosage form
               </div>
             </Col>
-            <Col sm={12} md={4} className="d-flex justify-content-between align-items-sm-center">
+            <Col
+              sm={12}
+              md={4}
+              className="d-flex justify-content-between align-items-sm-center"
+            >
               <div style={{ marginLeft: "5%" }}> Price * Quantity</div>
               <ButtonGroup style={{ marginRight: "10px" }}>
                 <Button variant="outline-secondary">-</Button>
